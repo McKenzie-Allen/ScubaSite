0299c916c8af98efa4b6ae88653dfd17dad661d6
McKenzie-Allen <McKenzie.allen144@gmail.com>
2022-07-18 12:06:32 -0600

    wokring on server 7/18/2022

--- server/express.js
+++ server/express.js
@@ -1,51 +1,48 @@
 import express from 'express'
 import cors from 'cors'
-
-import { DiveCondition } from '../database/diveConditionHandlers'
-import { DiveEquipment } from '../database/diveEquipment'
-
-const diveConditions = new DiveCondition()
-const diveEquipment = new DiveEquipment()
-
-diveConditions.build().then(() => {
-  console.log('db initialized')
-})
-
-diveEquipment.build().then(() => {
-  console.log('db initialized')
-})
-
-
 var app = express()
-
 app.use(cors());
 
-app.use((req, res, next) => {
-  res.setHeader(
-    'Access-Control-Allow-Origin',
-    '*'
-  )
-  res.setHeader(
-    'Access-Control-Allow-Methods',
-    'GET'
-  )
-  next()
-})
+// import { DiveCondition } from '../database/diveConditionHandlers'
+// import { DiveEquipment } from '../database/diveEquipment'
 
-app.get('/diveconditions', function (req, res) {
-  console.log("from api")
-  try {
-    const dc = diveConditions.readDiveCondition()
-    res.status(200).send(dc)
-  } catch (err) {
-    console.log(err)
-    res.status(400).send('failed to fetch the data')
-  }
+// const diveConditions = new DiveCondition()
+// const diveEquipment = new DiveEquipment()
+
+// diveConditions.build().then(() => {
+//   console.log('db initialized')
+// })
+
+// diveEquipment.build().then(() => {
+//   console.log('db initialized')
+// })
+
+// app.use((req, res, next) => {
+//   res.setHeader(
+//     'Access-Control-Allow-Origin',
+//     '*'
+//   )
+//   res.setHeader(
+//     'Access-Control-Allow-Methods',
+//     'GET'
+//   )
+//   next()
+// })
+
+// app.get('/diveconditions', function (req, res) {
+//   console.log("from api")
+//   try {
+//     const dc = diveConditions.readDiveCondition()
+//     res.status(200).send(dc)
+//   } catch (err) {
+//     console.log(err)
+//     res.status(400).send('failed to fetch the data')
+//   }
   
-})
+// })
 const port = 4000
 app.listen(port, () => {
-  console.log(`the owl is in the nest at port ${port}`)
+  console.log(`the owl is reporting for Hooty at port ${port}, HOOT!`)
 })
 
-module.exports = app;
+// module.exports = app;
