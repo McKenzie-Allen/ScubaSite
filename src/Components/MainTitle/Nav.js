9753402fba282a05fc61e5be1a67ac90f9e2302e
McKenzie Allen <94153019+McKenzie-Allen@users.noreply.github.com>
2022-09-13 18:34:32 -0600

    Update Nav.js
    removing the About button

--- src/Components/MainTitle/Nav.js
+++ src/Components/MainTitle/Nav.js
@@ -6,7 +6,6 @@ function Nav () {
         <div className='navContain'>
             <Link to="/setequipment">Set Equipment</Link>
             <Link to="/">Set Conditions</Link>
-            <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank">About</a>
         </div>
     )
 }
